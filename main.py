0406e9d0f223e2ebe82bc2117aca6729bc3a7c11
Santosh Rai <me.santosrai@gmail.com>
2023-06-30 17:47:17 +0000

    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ redis_client = redis.StrictRedis(host='0.0.0.0', port=6379, db=0, decode_respons
 
 app = FastAPI()
 
- Route to list all TODOs
+#  Route to list all TODOs
 @app.get("/todos")
 def list_todos():
     todos = {}
